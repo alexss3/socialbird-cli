b2172c4b69f63f1d9b1f2301d8c46996a457c4af
alexss3 <stultzat@gmail.com>
2020-07-23 14:36:26 +0200

    Fixed depedency typo in /cmds/read.js

--- cmds/read.js
+++ cmds/read.js
@@ -2,7 +2,7 @@ const { getPosts } = require('../utils/db');
 const postUtils = require('../utils/post');
 
 module.exports = (username) => {
-    return db.getPosts(username).map(post => {
+    return getPosts(username).map(post => {
         return postUtils.returnFormattedPost(post);
     });
 };
