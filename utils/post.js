bcf7a46d1a4c360155307e6db05b3cff38c0b612
alexss3 <stultzat@gmail.com>
2020-07-23 15:15:49 +0200

    Fixed formatting of time strings

--- utils/post.js
+++ utils/post.js
@@ -9,25 +9,30 @@ const returnTimeSince = (timestamp) => {
     const delta = (now - timestamp) / 1000;
 
     if (delta < 60) {
+        const s = (delta > 1) ? 's' : '';
 
-        return `${Math.floor(delta)} seconds ago`;
+        return `${Math.floor(delta)} second${s} ago`;
 
     } else if (delta > 60 && delta < 3600) {
 
         const minutes = Math.floor(delta/60);
+        const s = (minutes > 1) ? 's' : '';
 
-        return `${minutes} minutes ago`;
+        return `${minutes} minute${s} ago`;
     } else if (delta > 3600 && delta < 86400) {
 
         const hours = Math.floor(delta/60/60);
-        return `${hours} hours ago`;
+        const s = (hours > 1) ? 's' : '';
+        return `${hours} hour${s} ago`;
 
     } else {
 
         const days = Math.floor(delta/60/60/24);
-        return `${days} days ago`;
+        const s = (days > 1) ? 's' : '';
+        return `${days} day${s} ago`;
 
-    }
+    } 
+    // ... weeks, months, years
 }
 
 module.exports.returnTimeSince = returnTimeSince;
